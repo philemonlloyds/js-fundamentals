4f2975b568821391910cd80fc79692d2c3e43df3
philemon selvaraj <phil.lloyds@hotmail.com>
2016-10-10 05:00:27 +0000

    Js practice files

---
+++ ingredients.js
@@ -0,0 +1,8 @@
+
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+
+for(i=(ingredients.length)- 1;i>0;i--){
+    console.log(ingredients[i]);
+}
